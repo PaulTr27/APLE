e55363a6f262b7a842b0dfe22ce08c40e2f415b1
Paul Tran <phaolothienphu0910@gmail.com>
2022-12-02 15:38:56 +0700

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template, request
+
+
+
+app = Flask(__name__,static_folder="static")
+
+@app.route('/en',methods=["GET"])
+def en_main_page(language = "en"):
+    template_path = f"./{language}/index.html"
+    return render_template(template_path)
+
+@app.route('/vi', methods=["GET"])
+def vi_main_page(language = "vi"):
+    template_path = f"./{language}/index.html"
+    return render_template(template_path)    
+
+
+@app.route('/')
+def main():
+    template_path = "./vi/index.html"
+    return render_template(template_path)
